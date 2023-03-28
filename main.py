9f68bee7a73e5303c13b8cbc4630865e1f93ea54
aytid <124620798+aytid@users.noreply.github.com>
2023-03-28 23:08:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+st.title("ATTENDANCE")
+t=st.number_input("Enter total number of classes",min_value=1)
+p=st.number_input("Enter number of classes present",min_value=1)
+st.subheader("Present Attendance")
+a=round((p/t)*100,2)
+st.write(a)
+st.subheader("BUNK Meter")
+b=st.number_input(" ",min_value=0)
+st.subheader("Attendance after Bunking")
+t+=b
+a=round((p/t)*100,2)
+st.write(a)
+st.subheader("Enter target attendance")
+tg=st.number_input("   ",min_value=0)
+x=0
+while round(((p+x)/(t+x)*100),2)<tg:
+	x+=1
+st.write("Attend ",x," more Classes!!")
